b42da0d16772fabe04b61e9b682601b23ec87ef0
micketeer <micketeer@a35166fa-9c42-0410-8674-557589bd224c>
2008-03-22 12:32:12 +0000

    Cleaning up

--- trunk/pidginTeX.h
+++ trunk/pidginTeX.h
@@ -1,14 +1,10 @@
-/*
- * pidgin-mimetex.h 
- *
+/* pidgin-mimetex.h 
  * Based on:
  * LaTeX.c
  * pidgin-latex plugin
  *
  * This a plugin for Pidgin to display LaTeX formula in conversation
- *
  * PLEASE, send any comment, bug report, etc. to the trackers at sourceforge.net
- *
  * Copyright (C) 2006-2007 Benjamin Moll (qjuh@users.sourceforge.net)
  * some portions : Copyright (C) 2004-2006 Nicolas Schoonbroodt (nicolas@ffsa.be)
  *                 Copyright (C) 2004-2006 GRIm@ (thegrima@altern.org).
@@ -16,21 +12,17 @@
  * Windows port  : Copyright (C) 2005-2006 Nicolai Stange (nic-stange@t-online.de)
  * Other portions heavily inspired and copied from gaim sources
  * Copyright (C) 1998-2007 Pidgin developers pidgin.im
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; This document is under the scope of
  * the version 2 of the License, or any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program (see COPYING); if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 #ifndef _PIDGINTEX_H_
 #define _PIDGINTEX_H_
@@ -44,21 +36,32 @@
 
 #define PURPLE_PLUGINS
 
+#ifdef ENABLE_NLS
+#  include <locale.h>
+#  include <libintl.h>
+#  define _(String) dgettext (PLUGIN_NAME, String)
+// TODO:
+#  define PP_LOCALEDIR "locale"
+#else
+#  define _(String) (String)
+#endif
+
 // Includes
 #ifdef _WIN32
 #define g_file_get_contents g_file_get_contents_utf8
 #include <windows.h>
 #endif
 
-#include <libpurple/conversation.h>
-#include <libpurple/debug.h>
-#include <libpurple/signals.h>
-#include <libpurple/imgstore.h>
-#include <libpurple/util.h>
-#include <libpurple/notify.h>
-#include <libpurple/server.h>
-#include <libpurple/log.h>
-#include <libpurple/version.h>
+#include <conversation.h>
+#include <debug.h>
+#include <signals.h>
+#include <imgstore.h>
+#include <util.h>
+#include <notify.h>
+#include <server.h>
+#include <log.h>
+#include <version.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
